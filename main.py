a0fb07f9102c8f32e4f7ce3dd8209cbed4246631
Somen <somen4898@gmail.com>
2023-10-12 12:19:06 +0530

    can now multiply with negative numbers and deal with questions where multiple different operators are used where it only takes the last operator

--- main.py
+++ main.py
@@ -1,103 +1,36 @@
 import re
 
+def clean_expression(expression):
+    # Use regular expression to remove consecutive / and - operators
+    cleaned_expression = re.sub(r'([/])\1+', r'\1', expression)
+    cleaned_expression = re.sub(r'([-])\1+', r'\1', cleaned_expression)
+    cleaned_expression = re.sub(r'([*])\1+', r'\1', cleaned_expression)
+    cleaned_expression = re.sub(r'([+])\1+', r'\1', cleaned_expression)
 
-def is_valid_expression(exp):
-    return (exp.count("(") == exp.count(")")) and re.match(r'^[0-9+\-*/() ]*$', exp)
+    cleaned_expression = re.sub(r'([-+])\1+', '+', expression)
+    cleaned_expression = re.sub(r'([*+])\1+', '+', expression)
+    cleaned_expression = re.sub(r'([/+])\1+', '+', expression)
+    cleaned_expression = re.sub(r'([+*])\1+', '*', expression)
+    cleaned_expression = re.sub(r'([/*])\1+', '*', expression)
+    cleaned_expression = re.sub(r'([-*])\1+', '*', expression)
+    cleaned_expression = re.sub(r'([-/])\1+', '/', expression)
+    cleaned_expression = re.sub(r'([+/])\1+', '/', expression)
+    cleaned_expression = re.sub(r'([*/])\1+', '/', expression)
+    return cleaned_expression
 
-
-def tokenize(exp):
-    tokens = []
-    start = 0
-    prev_operator = None  # Variable to store the previous operator
-    for i, c in enumerate(exp):
-        if c in '+-/*()':
-            token_value = exp[start:i].strip()
-            if token_value:
-                tokens.append(token_value)
-            # Check if the current operator is the same as the previous one
-            if c == prev_operator:
-                pass  # Skip consecutive operators of the same type
-            else:
-                tokens.append(c)
-                prev_operator = c  # Update the previous operator
-            start = i + 1
-    token_value = exp[start:].strip()
-    if token_value:
-        tokens.append(token_value)
-    return [t for t in tokens if t != ""]
-
-
-
-def evaluate_parentheses(tokens):
-    stack = []
-    for i, token in enumerate(tokens):
-        if token == "(":
-            stack.append(i)
-        elif token == ")":
-            start_index = stack.pop()
-            result_inside_parentheses = calculate(tokens[start_index + 1:i])
-            result_tokens = [t for t in result_inside_parentheses if t != ""]
-            tokens = tokens[:start_index] + result_tokens + tokens[i + 1:]
-            return evaluate_parentheses(tokens)
-    return tokens
-
-
-def multiply_and_divide(tokens):
-    new_token = []
-    skip_token = False
-    for i, token in enumerate(tokens):
-        if skip_token:
-            skip_token = False
-            continue
-        if token == "*":
-            new_token.append(float(new_token.pop()) * float(tokens[i + 1]))
-            skip_token = True
-        elif token == "/":
-            if float(tokens[i + 1]) == 0:
-                return None
-            else:
-                new_token.append(float(new_token.pop()) / float(tokens[i + 1]))
-                skip_token = True
-        else:
-            new_token.append(token)
-    return new_token
-
-
-def add_and_sub(tokens):
-    new_token = []
-    skip_token = False
-    for i, token in enumerate(tokens):
-        if skip_token:
-            skip_token = False
-            continue
-        if token == '+':
-            new_token.append(float(new_token.pop()) + float(tokens[i + 1]))
-            skip_token = True
-        elif token == "-":
-            new_token.append(float(new_token.pop()) - float(tokens[i + 1]))
-            skip_token = True
-        else:
-            new_token.append(token)
-    return new_token
-
-
-def calculate(tokens):
-    tokens = evaluate_parentheses(tokens)
-    tokens = multiply_and_divide(tokens)
-    if tokens is None:
-        print("Value divided by zero is invalid. NAN")
-        return
-    tokens = add_and_sub(tokens)
-    return tokens
+def calculate(expression):
+    try:
+        cleaned_expression = clean_expression(expression)
+        # Evaluate the cleaned expression and return the result
+        result = eval(cleaned_expression)
+        return result
+    except Exception as e:
+        return str(e)
 
 
 while True:
-    exp = input("Enter string with operands +,-,*,/ or type 'stop' to stop: \n")
-    if exp.lower() == 'stop':
+    expression = input("Enter an expression (or 'stop' to exit):\n")
+    if expression == "stop":
         break
-    if is_valid_expression(exp):
-        tokens = tokenize(exp)
-        answer = calculate(tokens)
-        print(answer)
-    else:
-        print("NAN")
+    result = calculate(expression)
+    print(result)
